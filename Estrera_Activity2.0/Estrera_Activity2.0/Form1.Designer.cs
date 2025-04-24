ff6f828571ff21f1cf92ebfb869c69c5b34b5852
Rico Estrera <146191199+Rico-John-Paul-Estrera@users.noreply.github.com>
2025-04-24 13:22:33 +0800

    Add files via upload

---
+++ Estrera_Activity2.0/Estrera_Activity2.0/Form1.Designer.cs
@@ -0,0 +1,212 @@
+﻿namespace Estrera_Activity2._0
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.btnUpdate = new System.Windows.Forms.Button();
+            this.btnSubmit = new System.Windows.Forms.Button();
+            this.txtFname = new System.Windows.Forms.TextBox();
+            this.lblDisplay = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtMiddleName = new System.Windows.Forms.TextBox();
+            this.txtLname = new System.Windows.Forms.TextBox();
+            this.txtAge = new System.Windows.Forms.TextBox();
+            this.txtEmail = new System.Windows.Forms.TextBox();
+            this.txtCourse = new System.Windows.Forms.TextBox();
+            this.listDisplay = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(128)))));
+            this.btnDelete.Font = new System.Drawing.Font("Book Antiqua", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnDelete.Location = new System.Drawing.Point(620, 439);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(97, 34);
+            this.btnDelete.TabIndex = 24;
+            this.btnDelete.Text = "Delete";
+            this.btnDelete.UseVisualStyleBackColor = false;
+            this.btnDelete.Click += new System.EventHandler(this.btnDelete_Click);
+            // 
+            // btnUpdate
+            // 
+            this.btnUpdate.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
+            this.btnUpdate.Font = new System.Drawing.Font("Book Antiqua", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnUpdate.Location = new System.Drawing.Point(518, 438);
+            this.btnUpdate.Name = "btnUpdate";
+            this.btnUpdate.Size = new System.Drawing.Size(85, 34);
+            this.btnUpdate.TabIndex = 23;
+            this.btnUpdate.Text = "Update";
+            this.btnUpdate.UseVisualStyleBackColor = false;
+            this.btnUpdate.Click += new System.EventHandler(this.btnUpdate_Click);
+            // 
+            // btnSubmit
+            // 
+            this.btnSubmit.BackColor = System.Drawing.SystemColors.InactiveCaption;
+            this.btnSubmit.Font = new System.Drawing.Font("Book Antiqua", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSubmit.Location = new System.Drawing.Point(157, 439);
+            this.btnSubmit.Name = "btnSubmit";
+            this.btnSubmit.Size = new System.Drawing.Size(85, 33);
+            this.btnSubmit.TabIndex = 22;
+            this.btnSubmit.Text = "Submit";
+            this.btnSubmit.UseVisualStyleBackColor = false;
+            this.btnSubmit.Click += new System.EventHandler(this.sbtBtn_Click);
+            // 
+            // txtFname
+            // 
+            this.txtFname.Font = new System.Drawing.Font("Book Antiqua", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtFname.Location = new System.Drawing.Point(114, 152);
+            this.txtFname.Name = "txtFname";
+            this.txtFname.Size = new System.Drawing.Size(208, 26);
+            this.txtFname.TabIndex = 15;
+            this.txtFname.TextChanged += new System.EventHandler(this.txtFname_TextChanged);
+            // 
+            // lblDisplay
+            // 
+            this.lblDisplay.AutoSize = true;
+            this.lblDisplay.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.lblDisplay.Font = new System.Drawing.Font("Book Antiqua", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblDisplay.Location = new System.Drawing.Point(519, 102);
+            this.lblDisplay.Name = "lblDisplay";
+            this.lblDisplay.Size = new System.Drawing.Size(310, 33);
+            this.lblDisplay.TabIndex = 14;
+            this.lblDisplay.Text = "All Submitted Entries";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.label1.Font = new System.Drawing.Font("Book Antiqua", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(107, 102);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(327, 33);
+            this.label1.TabIndex = 13;
+            this.label1.Text = "Enter Your Information";
+            // 
+            // txtMiddleName
+            // 
+            this.txtMiddleName.Font = new System.Drawing.Font("Book Antiqua", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtMiddleName.Location = new System.Drawing.Point(113, 194);
+            this.txtMiddleName.Name = "txtMiddleName";
+            this.txtMiddleName.Size = new System.Drawing.Size(208, 26);
+            this.txtMiddleName.TabIndex = 25;
+            this.txtMiddleName.TextChanged += new System.EventHandler(this.txtMiddleName_TextChanged);
+            // 
+            // txtLname
+            // 
+            this.txtLname.Font = new System.Drawing.Font("Book Antiqua", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtLname.Location = new System.Drawing.Point(113, 244);
+            this.txtLname.Name = "txtLname";
+            this.txtLname.Size = new System.Drawing.Size(208, 26);
+            this.txtLname.TabIndex = 26;
+            this.txtLname.TextChanged += new System.EventHandler(this.txtLname_TextChanged);
+            // 
+            // txtAge
+            // 
+            this.txtAge.Font = new System.Drawing.Font("Book Antiqua", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtAge.Location = new System.Drawing.Point(113, 289);
+            this.txtAge.Name = "txtAge";
+            this.txtAge.Size = new System.Drawing.Size(208, 26);
+            this.txtAge.TabIndex = 27;
+            this.txtAge.TextChanged += new System.EventHandler(this.txtAge_TextChanged);
+            // 
+            // txtEmail
+            // 
+            this.txtEmail.Font = new System.Drawing.Font("Book Antiqua", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtEmail.Location = new System.Drawing.Point(114, 333);
+            this.txtEmail.Name = "txtEmail";
+            this.txtEmail.Size = new System.Drawing.Size(208, 26);
+            this.txtEmail.TabIndex = 28;
+            this.txtEmail.TextChanged += new System.EventHandler(this.txtEmail_TextChanged);
+            // 
+            // txtCourse
+            // 
+            this.txtCourse.Font = new System.Drawing.Font("Book Antiqua", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtCourse.Location = new System.Drawing.Point(113, 382);
+            this.txtCourse.Name = "txtCourse";
+            this.txtCourse.Size = new System.Drawing.Size(208, 26);
+            this.txtCourse.TabIndex = 29;
+            this.txtCourse.TextChanged += new System.EventHandler(this.txtCourse_TextChanged);
+            // 
+            // listDisplay
+            // 
+            this.listDisplay.Font = new System.Drawing.Font("Book Antiqua", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.listDisplay.FormattingEnabled = true;
+            this.listDisplay.HorizontalScrollbar = true;
+            this.listDisplay.ItemHeight = 20;
+            this.listDisplay.Location = new System.Drawing.Point(467, 152);
+            this.listDisplay.Name = "listDisplay";
+            this.listDisplay.Size = new System.Drawing.Size(457, 264);
+            this.listDisplay.TabIndex = 30;
+            this.listDisplay.SelectedIndexChanged += new System.EventHandler(this.listDisplay_SelectedIndexChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(1011, 574);
+            this.Controls.Add(this.listDisplay);
+            this.Controls.Add(this.txtCourse);
+            this.Controls.Add(this.txtEmail);
+            this.Controls.Add(this.txtAge);
+            this.Controls.Add(this.txtLname);
+            this.Controls.Add(this.txtMiddleName);
+            this.Controls.Add(this.btnDelete);
+            this.Controls.Add(this.btnUpdate);
+            this.Controls.Add(this.btnSubmit);
+            this.Controls.Add(this.txtFname);
+            this.Controls.Add(this.lblDisplay);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnDelete;
+        private System.Windows.Forms.Button btnUpdate;
+        private System.Windows.Forms.Button btnSubmit;
+        private System.Windows.Forms.TextBox txtFname;
+        private System.Windows.Forms.Label lblDisplay;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtMiddleName;
+        private System.Windows.Forms.TextBox txtLname;
+        private System.Windows.Forms.TextBox txtAge;
+        private System.Windows.Forms.TextBox txtEmail;
+        private System.Windows.Forms.TextBox txtCourse;
+        private System.Windows.Forms.ListBox listDisplay;
+    }
+}
+
---
+++ Estrera_Activity2.0/Estrera_Activity2.0/Form1.cs
@@ -0,0 +1,276 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Estrera_Activity2._0
+{
+    public partial class Form1 : Form
+    {
+        private const string FileName = "StudentInfo.txt";
+        private string FilePath;
+        private int selectedIndex = -1;
+
+        public Form1()
+        {
+            InitializeComponent();
+            FilePath = Path.Combine(@"Z:\II320\SEMI-FINAL\Estrera_Activity2.0\Estrera_Activity2.0\files", FileName);
+            Directory.CreateDirectory(Path.GetDirectoryName(FilePath));
+            SetPlaceholderText();
+        }
+
+        private void txtFname_TextChanged(object sender, EventArgs e) { }
+        private void txtMiddleName_TextChanged(object sender, EventArgs e) { }
+        private void txtLname_TextChanged(object sender, EventArgs e) { }
+        private void txtAge_TextChanged(object sender, EventArgs e) { }
+        private void txtEmail_TextChanged(object sender, EventArgs e) { }
+        private void txtCourse_TextChanged(object sender, EventArgs e) { }
+
+        private void sbtBtn_Click(object sender, EventArgs e)
+        {
+            string firstname = txtFname.Text.Trim();
+            string middleName = txtMiddleName.Text.Trim();
+            string lastname = txtLname.Text.Trim();
+            string ageText = txtAge.Text.Trim();
+            string email = txtEmail.Text.Trim();
+            string course = txtCourse.Text.Trim();
+            int age;
+
+            if (string.IsNullOrWhiteSpace(firstname) || string.IsNullOrWhiteSpace(middleName) ||
+                string.IsNullOrWhiteSpace(lastname) || string.IsNullOrWhiteSpace(ageText) ||
+                string.IsNullOrWhiteSpace(email) || string.IsNullOrWhiteSpace(course))
+            {
+                MessageBox.Show("Please fill in all fields.");
+                return;
+            }
+
+            try
+            {
+                age = Convert.ToInt32(ageText);
+                if (age < 0 || age > 150)
+                {
+                    MessageBox.Show("Please enter a valid age between 0 and 150.");
+                    return;
+                }
+            }
+            catch (FormatException)
+            {
+                MessageBox.Show("Please enter a valid integer for age.");
+                return;
+            }
+            catch (OverflowException)
+            {
+                MessageBox.Show("Please enter a valid age within the acceptable range.");
+                return;
+            }
+
+            if (!email.Contains("@") || !email.Contains("."))
+            {
+                MessageBox.Show("Please enter a valid email address.");
+                return;
+            }
+
+            string studentInfo = firstname + "   |   " + middleName + "   |   " + lastname + "   |   " + age + "   |   " + email + "   |   " + course;
+
+            try
+            {
+                using (StreamWriter sw = new StreamWriter(FilePath, true))
+                {
+                    sw.WriteLine(studentInfo);
+                }
+
+                ClearTextFields();
+                MessageBox.Show("Student information saved successfully!");
+                LoadStudentData();
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Error saving to file");
+            }
+        }
+
+        private void btnUpdate_Click(object sender, EventArgs e)
+        {
+            if (selectedIndex >= 0)
+            {
+                string firstname = txtFname.Text.Trim();
+                string middleName = txtMiddleName.Text.Trim();
+                string lastname = txtLname.Text.Trim();
+                string ageText = txtAge.Text.Trim();
+                string email = txtEmail.Text.Trim();
+                string course = txtCourse.Text.Trim();
+                int age;
+
+                if (string.IsNullOrWhiteSpace(firstname) || string.IsNullOrWhiteSpace(middleName) ||
+                   string.IsNullOrWhiteSpace(lastname) || string.IsNullOrWhiteSpace(ageText) ||
+                   string.IsNullOrWhiteSpace(email) || string.IsNullOrWhiteSpace(course))
+                {
+                    MessageBox.Show("Please fill in all fields.");
+                    return;
+                }
+
+                try
+                {
+                    age = Convert.ToInt32(ageText);
+                    if (age < 0 || age > 150)
+                    {
+                        MessageBox.Show("Please enter a valid age between 0 and 150.");
+                        return;
+                    }
+                }
+                catch (FormatException)
+                {
+                    MessageBox.Show("Please enter a valid integer for age.");
+                    return;
+                }
+                catch (OverflowException)
+                {
+                    MessageBox.Show("Please enter a valid age within the acceptable range.");
+                    return;
+                }
+
+                if (!email.Contains("@") || !email.Contains("."))
+                {
+                    MessageBox.Show("Please enter a valid email address.");
+                    return;
+                }
+
+                string updatedStudentInfo = firstname + "  |  " + middleName + "  |  " + lastname + "  |  " + age + "  |  " + email + "  |  " + course;
+                List<string> lines = File.ReadAllLines(FilePath).ToList();
+                lines[selectedIndex] = updatedStudentInfo;
+                File.WriteAllLines(FilePath, lines);
+
+                LoadStudentData();
+                MessageBox.Show("Record Updated");
+                ClearTextFields();
+            }
+            else
+            {
+                MessageBox.Show("Please select a student to update.");
+            }
+        }
+
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+            if (selectedIndex >= 0)
+            {
+                List<string> lines = File.ReadAllLines(FilePath).ToList();
+                lines.RemoveAt(selectedIndex);
+                File.WriteAllLines(FilePath, lines);
+
+                LoadStudentData();
+                MessageBox.Show("Record Deleted");
+                ClearTextFields();
+            }
+            else
+            {
+                MessageBox.Show("Please select a student to Delete.");
+            }
+        }
+
+        private void LoadStudentData()
+        {
+            listDisplay.Items.Clear();
+
+            try
+            {
+                string[] lines = File.ReadAllLines(FilePath);
+                foreach (string line in lines)
+                {
+                    listDisplay.Items.Add(line);
+                }
+            }
+            catch (FileNotFoundException)
+            {
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error reading from file: " + ex.Message);
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            LoadStudentData();
+        }
+
+        private void listDisplay_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (listDisplay.SelectedIndex >= 0)
+            {
+                selectedIndex = listDisplay.SelectedIndex;
+                string selectedStudent = listDisplay.SelectedItem.ToString();
+                string[] studentDetails = selectedStudent.Split(new[] { "   |   " }, StringSplitOptions.None);
+
+                if (studentDetails.Length == 6)
+                {
+                    txtFname.Text = studentDetails[0].Trim();
+                    txtMiddleName.Text = studentDetails[1].Trim();
+                    txtLname.Text = studentDetails[2].Trim();
+                    txtAge.Text = studentDetails[3].Trim();
+                    txtEmail.Text = studentDetails[4].Trim();
+                    txtCourse.Text = studentDetails[5].Trim();
+
+                    txtFname.ForeColor = Color.Black;
+                    txtMiddleName.ForeColor = Color.Black;
+                    txtLname.ForeColor = Color.Black;
+                    txtAge.ForeColor = Color.Black;
+                    txtEmail.ForeColor = Color.Black;
+                    txtCourse.ForeColor = Color.Black;
+                }
+            }
+        }
+
+        private void SetPlaceholderText()
+        {
+            SetPlaceholderForTextbox(txtFname, "First Name");
+            SetPlaceholderForTextbox(txtMiddleName, "Middle Name");
+            SetPlaceholderForTextbox(txtLname, "Last Name");
+            SetPlaceholderForTextbox(txtEmail, "Email Address");
+            SetPlaceholderForTextbox(txtCourse, "Course");
+            SetPlaceholderForTextbox(txtAge, "Age");
+        }
+
+        private void SetPlaceholderForTextbox(TextBox textBox, string placeholderText)
+        {
+            textBox.Text = placeholderText;
+            textBox.ForeColor = System.Drawing.Color.Gray;
+
+            textBox.Enter += (sender, e) =>
+            {
+                if (textBox.Text == placeholderText)
+                {
+                    textBox.Text = "";
+                    textBox.ForeColor = System.Drawing.Color.Black;
+                }
+            };
+
+            textBox.Leave += (sender, e) =>
+            {
+                if (string.IsNullOrWhiteSpace(textBox.Text))
+                {
+                    textBox.Text = placeholderText;
+                    textBox.ForeColor = System.Drawing.Color.Gray;
+                }
+            };
+        }
+
+        private void ClearTextFields()
+        {
+            txtFname.Clear();
+            txtMiddleName.Clear();
+            txtLname.Clear();
+            txtAge.Clear();
+            txtEmail.Clear();
+            txtCourse.Clear();
+        }
+
+        private void richTextBoxDisplay_TextChanged(object sender, EventArgs e) { }
+    }
+}
